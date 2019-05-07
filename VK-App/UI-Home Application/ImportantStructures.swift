5317e11fe4182c9c75cf2403a77b2e698542ecf2
MaKoSam <mazniker@icloud.com>
2019-05-07 20:17:21 +0800

    --Client&Server--Lesson 1-- 1) Session singleton created

--- VK-App/UI-Home Application/ImportantStructures.swift
+++ VK-App/UI-Home Application/ImportantStructures.swift
@@ -13,6 +13,17 @@ struct Person{
     var photo : UIImage
 }
 
+class Session {
+    static let instance = Session()
+    
+    private init(){
+    }
+    
+    var token: String = "Not Initialized"
+    var userId: Int = 404
+    
+}
+
 
 struct FriendData {
     var privateUserData = [
